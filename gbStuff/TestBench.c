ff9b0239ec373d3252065657ef427aa0b9782e06
Shrimpstone <104617276+Shrimpstone@users.noreply.github.com>
2024-04-16 15:40:32 +0200

    Create TestBench.c

---
+++ gbStuff/TestBench.c
@@ -0,0 +1,295 @@
+#include <stdio.h>
+#include <gb/gb.h>
+#include <gb/drawing.h>
+
+// OK BITCHBOY HERE IS WHAT YOU DO
+// cd Documents\VSCstuff\Projects\GB\Examples\"02. TestbedRead"
+// .\compile.bat
+
+void ClearScreen()
+{
+    color(WHITE, WHITE, SOLID);
+    box(0, 0, GRAPHICS_WIDTH, GRAPHICS_HEIGHT, M_FILL);
+    color(BLACK, WHITE, SOLID);
+}
+
+void Lines()
+{
+while (1)
+    {
+        color(BLACK, WHITE, SOLID);
+        switch (joypad())
+            {
+            case J_A:
+                line(0,0,GRAPHICS_WIDTH,GRAPHICS_HEIGHT);
+                break;
+            case J_B:
+                box(20,20,40,40,M_NOFILL);
+                break;
+            case J_UP:
+                circle(GRAPHICS_WIDTH/2, GRAPHICS_HEIGHT/2, 50, M_FILL);
+                break;
+            case J_DOWN:
+                ClearScreen();
+                break;
+
+            default:
+                break;
+            }
+    }
+}
+
+void Buttons()
+{
+    //Using ifs instead of switch case to allow for multiple button inputs at once
+    while (1)
+    {
+        uint8_t mButtons = joypad();
+
+        if (mButtons & J_A)
+        {
+            printf("A ");
+        }
+        if (mButtons & J_B)
+        {
+            printf("B ");
+        }
+        if (mButtons & J_DOWN)
+        {
+            printf("V ");
+        }
+        if (mButtons & J_LEFT)
+        {
+            printf("< ");
+        }
+        if (mButtons & J_RIGHT)
+        {
+            printf("> ");
+        }
+        if (mButtons & J_UP)
+        {
+            printf("^ ");
+        }
+        if (mButtons & J_START)
+        {
+            printf("+ ");
+        }
+        if (mButtons & J_SELECT)
+        {
+            printf("- ");
+        }
+
+        printf("\n");
+        while(mButtons == joypad());
+    }
+}
+
+const uint8_t angleLUTx[] = {
+    140, // 0 deg
+    136, // 15 deg - Approximation
+    132, // 30 deg
+    121, // 45 deg - Approximation
+    110, // 60 deg
+    95,  // 75 deg - Approximation
+    80,  // 90 deg
+    65,  // 105 deg - Approximation
+    50,  // 120 deg
+    35,  // 135 deg - Approximation
+    28,  // 150 deg
+    24,  // 165 deg - Approximation
+    20,  // 180 deg
+    24,  // 195 deg - Approximation
+    28,  // 210 deg
+    35,  // 225 deg - Approximation
+    50,  // 240 deg
+    65,  // 255 deg - Approximation
+    80,  // 270 deg
+    95,  // 285 deg - Approximation
+    110, // 300 deg
+    121, // 315 deg - Approximation
+    132, // 330 deg
+    136  // 345 deg - Approximation
+};
+
+const uint8_t angleLUTy[] = {
+    72, // 0 deg
+    57, // 15 deg - Approximation
+    42, // 30 deg
+    31, // 45 deg - Approximation
+    20, // 60 deg
+    16, // 75 deg - Approximation
+    12, // 90 deg
+    16, // 105 deg - Approximation
+    20, // 120 deg
+    31, // 135 deg - Approximation
+    42, // 150 deg
+    57, // 165 deg - Approximation
+    72, // 180 deg
+    87, // 195 deg - Approximation
+    102, // 210 deg
+    113, // 225 deg - Approximation
+    124, // 240 deg
+    128, // 255 deg - Approximation
+    132, // 270 deg
+    128, // 285 deg - Approximation
+    124, // 300 deg
+    113, // 315 deg - Approximation
+    102, // 330 deg
+    87  // 345 deg - Approximation
+};
+
+void Rotate()
+{
+    while (1)
+    {
+        size_t count = sizeof(angleLUTx) / sizeof(angleLUTx[0]);
+
+        for(size_t i = 0; i < count; ++i) {
+
+            size_t oppositeIndex = (i + count / 2) % count; // Get the opposite side of the index for end points of the line
+
+            color(BLACK, WHITE, SOLID);
+            line(angleLUTx[i], angleLUTy[i], angleLUTx[oppositeIndex], angleLUTy[oppositeIndex]);
+            delay(100);
+            color(WHITE, WHITE, SOLID);
+            line(angleLUTx[i], angleLUTy[i], angleLUTx[oppositeIndex], angleLUTy[oppositeIndex]);
+        }
+    }
+}
+
+void Control()
+{
+    size_t count = sizeof(angleLUTx) / sizeof(angleLUTx[0]);
+    size_t i = 0;           // Initialize angle index for LUTs
+    uint8_t yOffset = 0;    // Initialize y axis offset for multi axis movement
+    uint8_t yInc = 5;       // Increment of y offset
+
+    while(1)
+    {
+        vsync(); // Wait for the vertical blanking interval
+
+        uint8_t joypad_state = joypad();
+
+        if (joypad_state & J_RIGHT)
+        {
+            if(i == 0) i = count - 1;
+            else i--;
+        }
+        else if (joypad_state & J_LEFT)
+        {
+            i = (i + 1) % count;
+        }
+
+        // Vertical movement
+        if (joypad_state & J_UP)
+        {
+            yOffset += yInc;
+        }
+        if (joypad_state & J_DOWN)
+        {
+            yOffset -= yInc;
+        }
+        
+
+        size_t oppositeIndex = (i + count / 2) % count;
+
+        color(BLACK, WHITE, SOLID);
+        line(angleLUTx[i], angleLUTy[i] + yOffset, angleLUTx[oppositeIndex], angleLUTy[oppositeIndex] + yOffset);
+
+        while(joypad_state == joypad());    // Wait until new press is detected
+
+        color(WHITE, WHITE, SOLID);
+        line(angleLUTx[i], angleLUTy[i] + yOffset, angleLUTx[oppositeIndex], angleLUTy[oppositeIndex] + yOffset);
+    }
+}
+
+void Link() // github.com/mrombout/gbdk_playground/blob/master/link/link.c
+{
+    printf("A: Send\nB: Receive\n\n");
+	_io_out = 1;
+	while (1) {
+		if (joypad() == J_A) {
+            _io_out = 0x10; // Set IO out to send uint8_t value (EA)
+			waitpadup();
+			send_byte(); // send _io_out
+			printf("Sending...\n");
+			while (_io_status == IO_SENDING); // Wait for Send
+			if (_io_status == IO_IDLE) // If IO status returns to Idle then success
+				printf("Sent %d\n", (int)_io_out);
+			else
+				printf("Error\n"); // Else print error code
+			_io_out++;
+		}
+		if (joypad() == J_B) {
+			waitpadup();
+			receive_byte(); // receive _io_in
+			printf("Receiving...\n");
+			while (_io_status == IO_RECEIVING); // Wait for Receive
+			if (_io_status == IO_IDLE) // If IO status returns to Idle then success
+				printf("Received %d\n", (int)_io_in);
+			else
+				printf("Error\n"); // Else print error
+		}
+	}
+}
+
+void main()
+{
+    uint8_t mode = 0;
+
+    DISPLAY_ON;
+    set_bkg_data(0,0,NULL);
+    set_bkg_tiles(0,0,20,18,NULL);
+
+    printf("L - Buttons\nU - Drawing\nR - Rotate\nD - Control\n+ - Link\n");
+
+    while (mode == 0)
+    {
+        if (joypad() & J_LEFT) // if buttontest
+        {
+            mode = 1;
+        }
+        else if (joypad() & J_UP) // If drawingtest
+        {
+            mode = 2;
+        }
+        else if (joypad() & J_RIGHT)
+        {
+            mode = 3;
+        }
+        else if (joypad() & J_DOWN)
+        {
+            mode = 4;
+        }
+        else if (joypad() & J_START)
+        {
+            mode = 5;
+        }
+        
+        
+        else{}
+    }
+
+    if (mode == 1)
+    {
+        Buttons();
+    }
+    else if (mode == 2)
+    {
+        Lines();
+    }
+    else if (mode == 3)
+    {
+        Rotate();
+    }
+    else if (mode == 4)
+    {
+        Control();
+    }
+    else if (mode == 5)
+    {
+        Link();
+    }
+    
+    else{}
+}
