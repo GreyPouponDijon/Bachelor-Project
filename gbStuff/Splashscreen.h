a23a5b37228782159ac05c9d1ff93ba89329409f
Shrimpstone <104617276+Shrimpstone@users.noreply.github.com>
2024-05-02 11:56:23 +0200

    Add files via upload

---
+++ gbStuff/Splashscreen.h
@@ -0,0 +1,11 @@
+// Generated using GB Export by AngelSix https://github.com/angelsix/retrosix-resources
+// Enhanced version based of original by https://github.com/gabrielcnr
+
+#define Splashscreen_tilemap_width 0
+#define Splashscreen_tilemap_height 0
+
+#define Splashscreen_tileset_size 92
+
+extern const unsigned char Splashscreen_tileset[]; 
+
+extern const unsigned char Splashscreen_tilemap[];
