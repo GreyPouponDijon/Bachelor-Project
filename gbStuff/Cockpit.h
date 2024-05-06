09a6abb31f1b551c0f0220b698f263297cfa2bdc
Shrimpstone <104617276+Shrimpstone@users.noreply.github.com>
2024-05-06 16:26:10 +0200

    Add files via upload

---
+++ gbStuff/Cockpit.h
@@ -0,0 +1,11 @@
+// Generated using GB Export by AngelSix https://github.com/angelsix/retrosix-resources
+// Enhanced version based of original by https://github.com/gabrielcnr
+
+#define Cockpit_tilemap_width 0
+#define Cockpit_tilemap_height 0
+
+#define Cockpit_tileset_size 61
+
+extern const unsigned char Cockpit_tileset[]; 
+
+extern const unsigned char Cockpit_tilemap[];
